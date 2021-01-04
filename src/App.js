c2cec9548cb6df2cfe76e5ecc42131e85050a78a
techtwins <gshevaughn@gmail.com>
2021-01-04 17:58:37 -0500

    footer implemented

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import LoginForm from "./Components/LoginForm";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Jumbotron from "./Components/jumbotron";
-import jumboData from "./fixtures/jumbo.json";
+import { JumbotronContainer } from "./Container/Jumbotron";
+import { FooterContainer } from "./Container/Footer";
+
 class App extends Component {
   state = {
     CurrentUser: null,
@@ -12,19 +13,8 @@ class App extends Component {
     return (
       <Router>
         <LoginForm />
-        <Jumbotron.Container>
-          {jumboData.map((item) => (
-            <Jumbotron key={item.id} direction={item.direction}>
-              <Jumbotron.Pane>
-                <Jumbotron.Title>{item.title}</Jumbotron.Title>
-                <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
-              </Jumbotron.Pane>
-              <Jumbotron.Pane>
-                <Jumbotron.Image src={item.image} alt={item.alt} />
-              </Jumbotron.Pane>
-            </Jumbotron>
-          ))}
-        </Jumbotron.Container>
+        <JumbotronContainer />
+        <FooterContainer />
       </Router>
     );
   }
